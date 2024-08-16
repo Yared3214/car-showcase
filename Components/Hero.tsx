21bd550a89fcc3b823cf21629a0a5e530b37b021
Yared3214 <yaredman0099@gmail.com>
2024-08-16 01:19:50 -0700

    hero updated

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -7,9 +7,9 @@ const Hero = () => {
     const handleScroll = () => {
     }
   return (
-    <div className='flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto'>
+    <div className='flex lg:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto'>
         <div className="flex-1 pt-36 padding-x">
-            <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
+            <h1 className="lg:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
                 find, book, or rent car - quickly and easily
             </h1>
             <p className="text-[27px] text-black-100 font-light mt-5">
@@ -21,11 +21,11 @@ const Hero = () => {
         containerStyle="bg-primary-blue text-white rounded-full mt-10"
         handleClick={handleScroll}/>
         </div>
-        <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
-            <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0">
+        <div className="lg:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
+            <div className="relative lg:w-full w-[90%] lg:h-full h-[590px] z-0">
                 <Image src="/hero.png" alt='hero' fill className='object-contain'/>
             </div>
-                <div className='absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden'/>
+                <div className='absolute lg:-top-24 lg:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full lg:h-screen h-[590px] overflow-hidden'/>
         </div>
     </div>
   )
